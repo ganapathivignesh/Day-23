52d5e13832fa99367dd410936d0b0bb029f3d1dc
ganapathivignesh <69195663+ganapathivignesh@users.noreply.github.com>
2020-12-06 12:36:15 +0530

    HTTP session

---
+++ Httpsession/src/com/src/Sssion2.java
@@ -0,0 +1,56 @@
+package com.src;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Sssion2
+ */
+public class Sssion2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Sssion2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		HttpSession session=request.getSession(false);
+		String un=(String)session.getAttribute("u");
+		out.print("welcome to "+un);
+		out.print("<a href='servlet3'>logout</a>");
+		
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+	}
+	
+	
+
+}
